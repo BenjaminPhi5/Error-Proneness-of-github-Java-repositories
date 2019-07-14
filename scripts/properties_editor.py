e375f011a87dd9204f249078a2b35007decbd29f
ben philps <bp413@cam.ac.uk>
2019-07-14 15:00:44 +0100

    update gitignore

---
+++ scripts/properties_editor.py
@@ -0,0 +1,9 @@
+
+
+"""
+Edit the gradle.properties file to ensure that at least gradle 5.1 is being used.
+if not, modify the file to use gradle 5.4.1.
+
+
+For now, script is given a relative file handle when its main method is called.
+"""
